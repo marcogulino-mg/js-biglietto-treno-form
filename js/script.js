c3f6033eef4ba9fc0ab95f2404aa322cddd63b33
marco-gulino <marcogulino98@gmail.com>
2025-01-13 15:45:13 +0100

    [fix] remove "event" from preventDefault

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const form = document.querySelector("form");
 //Stampo il form tramite console.log al submit dei dati in input
 form.addEventListener("submit", (e) => {
     //Blocco il submit del form
-    event.preventDefault();
+    e.preventDefault();
 
     //Stampo il form
     console.log(`Numero di KM inseriti: ${kmNumber.value}, fascia di età selezionata: ${age.value}`);
