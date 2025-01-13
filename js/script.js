3c47faf0198ee02ad7d91c42924aaa73bec01f47
marco-gulino <marcogulino98@gmail.com>
2025-01-13 15:36:48 +0100

    [feat/js] print form values with console.log

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Seleziono tutti gli elementi del form
+const kmNumber = document.getElementById("kilometer-field");
+const age = document.getElementById("age-select");
+const form = document.querySelector("form");
+
+//Stampo il form tramite console.log al submit dei dati in input
+form.addEventListener("submit", (e) => {
+    //Blocco il submit del form
+    event.preventDefault();
+
+    //Stampo il form
+    console.log(`Numero di KM inseriti: ${kmNumber.value}, fascia di età selezionata: ${age.value}`);
+});
