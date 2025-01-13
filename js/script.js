01ebf2dc7de0f93976cc11b29037ccf1458abdd2
marco-gulino <marcogulino98@gmail.com>
2025-01-13 16:03:07 +0100

    [feat/js] create function that gives ticket final price

--- js/script.js
+++ js/script.js
@@ -3,11 +3,29 @@ const kmNumber = document.getElementById("kilometer-field");
 const age = document.getElementById("age-select");
 const form = document.querySelector("form");
 
+//Funzione per il calcolo del prezzo finale (0,21€ al Km | 20% per under 18 e 40% per over 65)
+function ticketPrice(ageUser, kilometer) {
+  let price = kilometer * 0.21;
+
+  if (ageUser === "underage") {
+    price -= (price * 20) / 100;
+  } else if (ageUser === "elderly") {
+    price -= (price * 40) / 100;
+  }
+
+  console.log(price);
+  return (Math.round(price * 100) / 100).toFixed(2);;
+}
+
 //Stampo il form tramite console.log al submit dei dati in input
 form.addEventListener("submit", (e) => {
-    //Blocco il submit del form
-    e.preventDefault();
+  //Blocco il submit del form
+  e.preventDefault();
 
-    //Stampo il form
-    console.log(`Numero di KM inseriti: ${kmNumber.value}, fascia di età selezionata: ${age.value}`);
+  //Calcolo il prezzo finale
+  let finalPrice = ticketPrice(age.value, kmNumber.value);
+
+  //Stampo il form
+  console.log(`Numero di KM inseriti: ${kmNumber.value}, fascia di età selezionata: ${age.value}.
+        Il prezzo finale ammonta a: ${finalPrice}€`);
 });
